a618311f44e74bd8a9341ebe546ba6cd15671ca5
Youssef Taoudi <Taoudi128@hotmail.com>
2021-11-11 00:04:57 +0100

    Loss functions added + training step completed, TODO: training loop

--- codebase/CycleGAN.py
+++ codebase/CycleGAN.py
@@ -2,36 +2,140 @@ from discriminator import Discriminator
 from generator import Generator
 from keras.layers import Input
 from keras.models import Model
+from keras.losses import MeanAbsoluteError, MeanSquaredError
+import tensorflow as tf
+#from keras.optimizers import Adam
+
+"""def generator_loss(gen):
+    return MeanSquaredError(tf.ones_like(gen), gen)
+
+
+def discriminator_loss(real,gen):
+    return (MeanSquaredError(tf.ones_like(real),real) + MeanSquaredError(tf.zeros_like(gen),gen))/2"""
 
 class CycleGAN:
-    def __init__(self):
+
+    def __init__(self,shape=((256, 256, 3))):#,batch):
+
+        x = Input(shape=shape)
+        y = Input(shape=shape)
+
+        #x,y = batch
         self.genG = Generator().model
         self.genF = Generator().model
         self.discX = Discriminator().model
         self.discY = Discriminator().model
 
-        inputX = Input(shape = (256, 256, 3)) 
-        inputY = Input(shape = (256, 256, 3)) 
+
+
+        self.genG_optimizer = tf.keras.optimizers.Adam(learning_rate=2e-4)
+        self.genF_optimizer = tf.keras.optimizers.Adam(learning_rate=2e-4)
+        self.discG_optimizer = tf.keras.optimizers.Adam(learning_rate=2e-4)
+        self.discF_optimizer = tf.keras.optimizers.Adam(learning_rate=2e-4)
+
+        self.cycle_weight = 10
+        self.identity_weight = 0.5
 
         # Reconstruction needed for Cycle Consistency loss
-        gen_imageY = self.genG(inputX)
-        gen_imageX = self.genF(inputY)
-        recon_imageX = self.genF(gen_imageY)
-        recon_imageY = self.genG(gen_imageX)
 
-        # Identity
-        gen_identityX = self.genF(inputX)
-        gen_identityY = self.genG(inputY)
+    def train_iteration(self,data_batch):
+        with tf.GradientTape(persistent=True) as tape:
+            x,y = data_batch
+            gen_y = self.genG(x)
+            gen_x = self.genF(y)
+            recon_x = self.genF(gen_y)
+            recon_y = self.genG(gen_x)
 
-        # disc
-        distinguishX = self.discX(gen_imageX)
-        distinguishY = self.discY(gen_imageY)
+            # Identity
+            identity_x = self.genF(x)
+            identity_y = self.genG(y)
 
-        self.discX.trainable = False
-        self.discY.trainable = False    
+            # disc
+            predict_x = self.discX(x)
+            predict_gen_x = self.discX(gen_x)
 
-        self.model = Model([inputX, inputY], [distinguishX, distinguishY, recon_imageX, recon_imageY, gen_identityX, gen_identityY])
+            predict_y = self.discY(y)
+            predict_gen_y = self.discY(gen_y)
+
+            def identity_loss(real, identity):
+                return MeanAbsoluteError(real, identity) * self.identity_weight * self.cycle_weight
+
+            def cycle_loss(real, recon):
+                return MeanAbsoluteError(real, recon) * self.cycle_weight
+
+            def gen_loss(predict):
+                return MeanSquaredError(tf.ones_like(predict), predict)
+
+            def disc_loss(predict_real, predict_gen):
+                return MeanSquaredError(predict_real,tf.ones_like(predict_real)) + predict_gen**2
+
+            self.discX.trainable = False
+            self.discY.trainable = False
+
+            G_cycle_loss = cycle_loss(x, recon_x)
+            F_cycle_loss = cycle_loss(y, recon_y)
+
+            G_identity_loss = identity_loss(y, identity_y)
+            F_identity_loss = identity_loss(x, identity_x)
+
+            G_gen_loss = gen_loss(predict_gen_y)
+            F_gen_loss = gen_loss(predict_gen_x)
+
+            G_disc_loss = disc_loss(predict_y, predict_gen_y)
+            F_disc_loss = disc_loss(predict_x,predict_gen_x)
+
+        gradsG = tape.gradient(G_cycle_loss+G_identity_loss+G_gen_loss, self.genG.trainable_variables)
+        gradsF = tape.gradient(F_cycle_loss+F_identity_loss+F_gen_loss, self.genF.trainable_variables)
+
+        discX_grads = tape.gradient(G_disc_loss, self.disc_X.trainable_variables)
+        discY_grads = tape.gradient(F_disc_loss, self.disc_Y.trainable_variables)
+
+
+        self.genG_optimizer.apply_gradients(
+            zip(gradsG, self.genG.trainable_variables)
+        )
+        self.genF_optimizer.apply_gradients(
+            zip(gradsF, self.genF.trainable_variables)
+        )
+
+        # Update the weights of the discriminators
+        self.discX_optimizer.apply_gradients(
+            zip(discX_grads, self.discX.trainable_variables)
+        )
+        self.discY_optimizer.apply_gradients(
+            zip(discY_grads, self.discY.trainable_variables)
+        )
+
+        return {
+            "G_loss": G_cycle_loss+G_identity_loss+G_gen_loss,
+            "F_loss": F_cycle_loss+F_identity_loss+F_gen_loss,
+            "D_X_loss": G_disc_loss,
+            "D_Y_loss": F_disc_loss,
+        }
+
+
+
+            
+
+
+
+
+
+       """ self.model = Model([x, y], [predict_gen_x, predict_gen_y, predict_x, predict_y, recon_x, recon_y, identity_x, identity_y])
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=2e-4)
+
+        self.discX.compile(loss='mse', optimizer=self.optimizer, metrics=['accuracy'])
+        self.discY.compile(loss='mse', optimizer=self.optimizer, metrics=['accuracy'])
+        self.model.compile(loss=['mse', 'mse', 'mae', 'mae', 'mae','mae'],optimizer=self.optimizer)"""
+
+    def train(data,epochs=10):
+        for _ in range(epochs):
+            for i in range(data.shape[0]):
+                img_a = np.ex
+
 
 
 if __name__ == '__main__':
-    CycleGAN().model.summary()
+    cgan = CycleGAN().model.summary()
+    #cgan.combine()
+    #cgan.model.summary()
--- codebase/generator.py
+++ codebase/generator.py
@@ -7,20 +7,20 @@ from keras.models import Model
 from reflection import ReflectionPadding2D
 
 class Generator:
-    def __init__(self,k=64,n_res=9):
+    def __init__(self,k=64,n_res=8):
         img_inp = Input(shape = (256, 256, 3))
-        c7s164 = ReflectionPadding2D()(img_inp)
+        c7s164 = ReflectionPadding2D(padding = (3,3))(img_inp)
         c7s164 = Conv2D(64,(7,7),(1,1))(c7s164)
         c7s164 = InstanceNormalization()(c7s164)
         c7s164 = ReLU()(c7s164)
 
-        d128 = ReflectionPadding2D()(c7s164)
-        d128 = Conv2D(128,(3,3),(2,2))(d128)
+        #d128 = ReflectionPadding2D()(c7s164)
+        d128 = Conv2D(128,(3,3),(2,2),padding="same")(c7s164)
         d128 = InstanceNormalization()(d128)
         d128 = ReLU()(d128)
 
-        d256 = ReflectionPadding2D()(d128)
-        d256 = Conv2D(256,(3,3),(2,2))(d256)
+        #d256 = ReflectionPadding2D()(d128)
+        d256 = Conv2D(256,(3,3),(2,2),padding="same")(d128)
         d256 = InstanceNormalization()(d256)
         d256 = ReLU()(d256)
 
@@ -41,19 +41,20 @@ class Generator:
             res = InstanceNormalization()(res)
             res = Concatenate()([res,curr])
             curr = res
+            
 
-        u128 = ReflectionPadding2D()(res)
-        u128 = Conv2DTranspose(128,(3,3),(2,2))(u128)
+        #u128 = ReflectionPadding2D()(res)
+        u128 = Conv2DTranspose(128,(3,3),(2,2),padding="same")(res)
         u128 = InstanceNormalization()(u128)
         u128 = ReLU()(u128)
 
-        u64 = ReflectionPadding2D()(u128)
-        u64 = Conv2DTranspose(64,(3,3),(2,2))(u64)
+        #u64 = ReflectionPadding2D()(u128)
+        u64 = Conv2DTranspose(64,(3,3),(2,2),padding="same")(u128)
         u64 = InstanceNormalization()(u64)
         u64 = ReLU()(u64)
 
-        c7s13 = ReflectionPadding2D()(u64)
-        c7s13 = Conv2D(3,(7,7),(1,1))(c7s13)
+        c7s13 = ReflectionPadding2D(padding=(3,3))(u64)
+        c7s13 = Conv2D(3,(7,7))(c7s13)
         c7s13 = InstanceNormalization()(c7s13)
         c7s13 = ReLU()(c7s13)
         self.model = Model(img_inp, c7s13)
